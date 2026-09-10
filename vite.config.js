27564a6279dfd3bfccb6653239b39df49d85baaf
Alexandr Calev <alexandrcalev@Alexandrs-MacBook-Pro.local>
2026-09-10 20:02:16 +0300

    Fix build and prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    minify: 'esbuild', // This silences the Lightning CSS Tailwind warnings
+  },
 })
